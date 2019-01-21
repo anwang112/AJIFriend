15360c54e798e1f4ec90ad67b09002b859e36ee5
lian0103 <k1319900103@gmail.com>
2019-01-21 15:31:22 +0800

    0121 by lien

--- js/main.js
+++ js/main.js
@@ -15,11 +15,11 @@ console.log(chatboxLeft);
 chatRoom_control.addEventListener('click',function(){
     if(ch1==0){//如果視窗是關閉狀態，就打開
         chatRoom.style.cssText="transform:translateY(0%)";
-        chatboxLeft.style.cssText="transform:translateY(0%);opacity:0;";
+        chatboxLeft.style.cssText="opacity:0;";
         return ch1 =1;
     }else{
         chatRoom.style.cssText="transform:translateY(77%)";
-        chatboxLeft.style.cssText="transform:translateY(77%);opacity:0;"; 
+        chatboxLeft.style.cssText="opacity:0;"; 
         return ch1 =0,ch=0;      
     }
 
@@ -28,11 +28,28 @@ chatRoom_control.addEventListener('click',function(){
 //聊天室左側區塊被點擊後要顯示出完整視窗，反之已顯示則關閉
 chatroom_btn_open.addEventListener('click',function(){
     // chatboxLeft.style.transform='translateX(-100%)';
-    if(ch==0){
-        chatboxLeft.style.cssText="transform:translateX(-100%);opacity:1;";
+    if(ch==0){//要顯示出來
+        chatboxLeft.style.cssText="left:-80%;opacity:1;";
+        // chatboxLeft.style.top="40%";
         return ch = 1;
-    }else{
-        chatboxLeft.style.cssText="transform:translateX(0%);opacity:0;";
+    }else{//要關閉
+        chatboxLeft.style.cssText="left:0%;opacity:0;";
+        // chatboxLeft.style.top="40%";
         return ch = 0;
     }
 });
+
+
+//spotlight控制
+
+var spotLight_01=document.getElementsByClassName('spotLight_01');
+console.log(spotLight_01);
+
+for(var i=0;i<spotLight_01.length;i++){
+    spotLight_01[i].addEventListener('mouseover',spotlight);
+}
+
+function spotlight(){
+    style='';
+    this.style.cssText="width:100px;transform:rotate(13deg);";
+}
