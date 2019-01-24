fab2fc4a07691b339fa186e3123781e5d99d3724
lian0103 <k1319900103@gmail.com>
2019-01-24 18:59:45 +0800

    0124共版

--- js/common.js
+++ js/common.js
@@ -1,66 +1,17 @@
 //產生head與聊天室
 function Pageini(){
     var commomPart =document.getElementById('commomPart');
-    var str_mm=`
-      <div class="wrap">
-      <!-- head頁首 -->
-          <div class="head">
-              <a href="#"><img id="logo" src="pic/logo.svg" alt="logo"></a>
-              <ul class="menu">
-                  <li><a href="#">找麻吉</a></li>
-                  <li><a href="#">活動巴士</a></li>
-                  <li><a href="#">造型商城</a></li>
-                  <li><a href="#">照片牆</a></li>
-              </ul>
-              <ul class="menu_sub">
-                  <li><a href="#">登入</a></li>
-                  <li><a href="#">魅力值</a></li>
-                  <li><a id="head_member_icon" href="#"><img  src="pic/head_member_icon.svg" alt="會員"></a></li>
-              </ul>
-          </div>
-      <!-- chatRoom聊天室 -->
-          <div id="chatRoom" class="chatRoom">
-              <!-- 聊天室右側主要顯示區  -->
-              <h2 id="chatRoom_control">麻吉聊天室</h2>
-              <div class="chatRoom_info">
-                  <img id="friendPic" src="pic/friend_demo_03.svg" alt="朋友大頭照">
-                  <span>吐2</span>
-                  <a href="#"><img src="pic/chatroom_btn_gift.svg" alt="送禮物按鍵"></a>
-                  <a href="#"><img src="pic/chatroom_btn_profile.svg" alt="查看個人檔案按鍵"></a>
-              </div> 
-              <div class="chatboxRight">
-                  <div class="chatbox_show">
-                      <img id="friendPicSmall" src="pic/friend_demo_03.svg" alt="朋友大頭照">
-                      <span>聊天內容</span>
-                  </div>
-                  <div class="chatbox_faces">
-                      <img src="pic/chatroom_face_01.svg" alt="喜">
-                      <img src="pic/chatroom_face_02.svg" alt="怒">
-                      <img src="pic/chatroom_face_03.svg" alt="哀">
-                      <img src="pic/chatroom_face_04.svg" alt="樂">
-                  </div>
-                  <div class="chatbox_input">
-                      <input id="chatTxt_input" type="text">
-                      <input id="chatTxt_send" type="button" value="送出">
-                  </div>      
-              </div> 
-              <!-- 聊天室收合左側欄  -->
-              <a href="#"><img id="chatroom_btn_open" src="pic/chatroom_btn_open1.svg" alt="收合左側欄按鍵"></a>
-              <div id="chatboxLeft" class="chatboxLeft">
-                  <input id="search_input" type="text" placeholder="搜尋好友">
-                  <div class="friendbox">
-                  </div>
-                  <div class="replybox">
-                      <h4>待回覆好友邀請</h3>
-                  </div>
-              </div>
-          </div>
-      <!-- footer頁尾 -->
-          <div class="footer"></div>
-      <!-- wrap結束 -->
-      </div>
+    var str_head=`
+      
     
     `;
+
+    var str_chatroom=`
+      
+    
+    `;
+
+
     commomPart.innerHTML=str_mm;
 }
 
--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 
 
-//取得按鍵
+//聊天室操控
 var chatRoom_control = document.getElementById('chatRoom_control');
 var chatroom_btn_open = document.getElementById('chatroom_btn_open');
 //取得物件
@@ -27,29 +27,76 @@ chatRoom_control.addEventListener('click',function(){
 
 //聊天室左側區塊被點擊後要顯示出完整視窗，反之已顯示則關閉
 chatroom_btn_open.addEventListener('click',function(){
-    // chatboxLeft.style.transform='translateX(-100%)';
     if(ch==0){//要顯示出來
-        chatboxLeft.style.cssText="left:-80%;opacity:1;";
-        // chatboxLeft.style.top="40%";
+        chatboxLeft.style.cssText="opacity:1;";
         return ch = 1;
     }else{//要關閉
-        chatboxLeft.style.cssText="left:0%;opacity:0;";
-        // chatboxLeft.style.top="40%";
+        chatboxLeft.style.cssText="opacity:0;";
         return ch = 0;
     }
 });
 
 
+//手機版聊天室操控_01
+var btn_chatroom_phone = document.getElementById('btn_chatroom_phone');
+var chatRoom_phone_part1 = document.getElementById('chatRoom_phone_part1');
+var chatRoom_phone_part2 = document.getElementById('chatRoom_phone_part2');
+var fr_demo = document.getElementById('fr_demo');
+var btn_menu_menu =document.getElementById('btn_menu_menu');
+var menu_phone = document.getElementById('menu_phone');
+var btn_chat_prev =document.getElementById('btn_chat_prev');
+
+
+btn_chatroom_phone.addEventListener('click',function(){
+    if(chatRoom_phone_part1.hasAttribute('value') == false){
+        chatRoom_phone_part1.style.cssText=" top: 8vh;;opacity:1";
+        chatRoom_phone_part2.style.cssText="opacity:0";
+        chatRoom_phone_part1.setAttribute('value','打開');
+    }else{
+        chatRoom_phone_part1.style.cssText=" top: -100vh;;opacity:0";
+        chatRoom_phone_part2.style.cssText="opacity:0";
+        chatRoom_phone_part1.removeAttribute('value','打開');
+    }
+
+},false);
+
+
+//手機版聊天室操控_02
+fr_demo.addEventListener('click',function(){
+    chatRoom_phone_part1.style.cssText=" top:8vh;left:0%;opacity:1; ";
+    chatRoom_phone_part2.style.cssText=" left: 0;display:flex;opacity:1;z-index:2";
+});
+
+btn_chat_prev.addEventListener('click',function(){
+    chatRoom_phone_part1.style.cssText=" top:8vh;left:0;opacity:1; ";
+    chatRoom_phone_part2.style.cssText="opacity:0;z-index:-1";
+},false);
+
+
+//手機版menu操控
+btn_menu_menu.addEventListener('click',function(){
+
+    if(menu_phone.hasAttribute('value') == false){
+        menu_phone.style.cssText="transform: scale(1);opacity:1; ";
+        menu_phone.setAttribute('value','打開');
+    }else{
+        menu_phone.style.cssText="transform: scale(0);opacity:0; ";
+        menu_phone.removeAttribute('value','打開');
+    }
+});
+
+
+
 //spotlight控制
 
-var spotLight_01=document.getElementsByClassName('spotLight_01');
-console.log(spotLight_01);
+// var spotLight_01=document.getElementsByClassName('spotLight_01');
+// console.log(spotLight_01);
 
-for(var i=0;i<spotLight_01.length;i++){
-    spotLight_01[i].addEventListener('mouseover',spotlight);
-}
+// for(var i=0;i<spotLight_01.length;i++){
+//     spotLight_01[i].addEventListener('mouseover',spotlight);
+// }
 
-function spotlight(){
-    style='';
-    this.style.cssText="width:100px;transform:rotate(13deg);";
-}
+// function spotlight(){
+//     style='';
+//     this.style.cssText="width:100px;transform:rotate(13deg);";
+// }
