efc986517f112bb144ceed89bf0df88869b80c31
anwang112 <anwang112@gmail.com>
2019-01-27 12:16:27 +0800

    0127-1

--- js/index.js
+++ js/index.js
@@ -50,12 +50,12 @@ var controller = new ScrollMagic.Controller();
 
 var tl2 = new TimelineMax();
 tl2.to('#indexSec', 1, {
- y: '-120%'
+    y: '-120%'
 });
 
 var scene04 = new ScrollMagic.Scene({
         triggerElement: '#trigger01',
-        duration: '100%',
+        duration: '80%',
         triggerHook: 0,
 
         // reverse: true
@@ -77,92 +77,120 @@ var $showhead = $('#bodyWrap');
 
 // 當滑鼠移到 .abgne-block-20120106 中的某一個超連結時
 var shake = function () {
-        // 把 #show-image 的 src 改成被移到的超連結的位置
-        var tis = new TimelineMax({});
-        tis.to($showhead, .4, {
+    // 把 #show-image 的 src 改成被移到的超連結的位置
+    var tis = new TimelineMax({});
+    tis.to($showhead, .4, {
 
-                    y: -100,
-                    repeat: 1,
-                    yoyo: true,
-                    ease: Power4.easeInOut,
+        y: -100,
+        repeat: 1,
+        yoyo: true,
+        ease: Power4.easeInOut,
 
-                });
-            };
+    });
+};
 
-                $('.bearHead').click(function () {
-                    // 把 #show-image 的 src 改成被移到的超連結的位置
-                    $showhead.css('background-image', 'url(../images/body1.png)');
-                    shake();
-                }); $('.rabiHead').click(function () {
-                    // 把 #show-image 的 src 改成被移到的超連結的位置
-                    $showhead.css('background-image', 'url(../images/body3.png)');
-                    shake();
-                }); $('.catHead').click(function () {
-                    // 把 #show-image 的 src 改成被移到的超連結的位置
-                    $showhead.css('background-image', 'url(../images/body2.png)');
-                    shake();
-                }); $('.dogHead').click(function () {
-                    // 把 #show-image 的 src 改成被移到的超連結的位置
-                    $showhead.css('background-image', 'url(../images/body4.png)');
-                    shake();
-                });
-                var $showeye = $('#eyes'); $('.eye1').click(function () {
-                    // 把 #show-image 的 src 改成被移到的超連結的位置
-                    $showeye.attr('src', 'images/eye1.png');
-                }); $('.eye2').click(function () {
-                    // 把 #show-image 的 src 改成被移到的超連結的位置
-                    $showeye.attr('src', 'images/eye2.png');
-                }); $('.eye3').click(function () {
-                    // 把 #show-image 的 src 改成被移到的超連結的位置
-                    $showeye.attr('src', 'images/eye3.png');
-                }); $('.eye4').click(function () {
-                    // 把 #show-image 的 src 改成被移到的超連結的位置
-                    $showeye.attr('src', 'images/eye4.png');
-                });
+$('.bearHead').click(function () {
+    // 把 #show-image 的 src 改成被移到的超連結的位置
+    $showhead.css('background-image', 'url(images/body1.png)');
+    shake();
+});
+$('.rabiHead').click(function () {
+    // 把 #show-image 的 src 改成被移到的超連結的位置
+    $showhead.css('background-image', 'url(images/body3.png)');
+    shake();
+});
+$('.catHead').click(function () {
+    // 把 #show-image 的 src 改成被移到的超連結的位置
+    $showhead.css('background-image', 'url(images/body2.png)');
+    shake();
+});
+$('.dogHead').click(function () {
+    // 把 #show-image 的 src 改成被移到的超連結的位置
+    $showhead.css('background-image', 'url(images/body4.png)');
+    shake();
+});
+var $showeye = $('#eyes');
+$('.eye1').click(function () {
+    // 把 #show-image 的 src 改成被移到的超連結的位置
+    $showeye.attr('src', 'images/eye1.png');
+});
+$('.eye2').click(function () {
+    // 把 #show-image 的 src 改成被移到的超連結的位置
+    $showeye.attr('src', 'images/eye2.png');
+});
+$('.eye3').click(function () {
+    // 把 #show-image 的 src 改成被移到的超連結的位置
+    $showeye.attr('src', 'images/eye3.png');
+});
+$('.eye4').click(function () {
+    // 把 #show-image 的 src 改成被移到的超連結的位置
+    $showeye.attr('src', 'images/eye4.png');
+});
 
-                $('.eyesChoice').hide(); $('.colorChoice').hide(); $('.eyesCir').click(function () {
-                    // 把 #show-image 的 src 改成被移到的超連結的位置
-                    $('.bodyChoice').hide();
-                    $('.eyesChoice').show();
-                    $('.colorChoice').hide();
-                }); $('.bodyCir').click(function () {
-                    // 把 #show-image 的 src 改成被移到的超連結的位置
-                    $('.bodyChoice').show();
-                    $('.eyesChoice').hide();
-                    $('.colorChoice').hide();
-                }); $('.colorCir').click(function () {
-                    // 把 #show-image 的 src 改成被移到的超連結的位置
-                    $('.bodyChoice').hide();
-                    $('.eyesChoice').hide();
-                    $('.colorChoice').show();
-                });
+$('.eyesChoice').hide();
+$('.colorChoice').hide();
+$('.eyesCir').click(function () {
+    // 把 #show-image 的 src 改成被移到的超連結的位置
+    $('.bodyChoice').hide();
+    $('.eyesChoice').show();
+    $('.colorChoice').hide();
+});
+$('.bodyCir').click(function () {
+    // 把 #show-image 的 src 改成被移到的超連結的位置
+    $('.bodyChoice').show();
+    $('.eyesChoice').hide();
+    $('.colorChoice').hide();
+});
+$('.colorCir').click(function () {
+    // 把 #show-image 的 src 改成被移到的超連結的位置
+    $('.bodyChoice').hide();
+    $('.eyesChoice').hide();
+    $('.colorChoice').show();
+});
 
 
 
 
 
-                var tllC = new TimelineMax({
-                    repeat: -1,
-                }); tllC.to('.lC1', .5, {
-                    opacity: 0.8,
+var tllC = new TimelineMax({
+    repeat: -1,
+});
+tllC.to('.lC1', .5, {
+    opacity: 0.8,
 
-                }).to('.lC2', .5, {
-                    opacity: 0.8,
+}).to('.lC2', .5, {
+    opacity: 0.8,
 
-                }).to('.lC3', .5, {
-                    opacity: 0.8,
+}).to('.lC3', .5, {
+    opacity: 0.8,
 
-                }).to('.lC4', .5, {
-                    opacity: 0.8,
+}).to('.lC4', .5, {
+    opacity: 0.8,
 
-                }).to('.lC5', .5, {
-                    opacity: 0.8,
+}).to('.lC5', .5, {
+    opacity: 0.8,
 
-                }).to('.lC6', .5, {
-                    opacity: 0.8,
+}).to('.lC6', .5, {
+    opacity: 0.8,
 
-                }).to('.littleChat', 1, {
-                    opacity: 0,
-                    y: -40,
+}).to('.littleChat', 1, {
+    opacity: 0,
+    y: -40,
 
-                });
+});
+
+$('.matchObj').click(function(){
+    $('.right').toggleClass('right').toggleClass('middle');
+    $('.left').toggleClass('left').addClass('right');
+    $(this).addClass('left').removeClass('middle');
+});
+
+// $("#aaa").fullpage({
+
+//     navigation: true, // 顯示導行列
+//     navigationPosition: "right" // 導行列位置
+
+// });
+
+
+
