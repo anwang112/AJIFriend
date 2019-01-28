ba422310a0a9daae71e6d16cceed203fbf1ad8b1
lian0103 <k1319900103@gmail.com>
2019-01-28 18:53:25 +0800

    0128-7

--- js/commonPart.js
+++ js/commonPart.js
@@ -15,7 +15,7 @@ function head_html(){
 	        <li><a href="photo.html">照片牆</a></li>
 	    </ul>
 	    <ul class="menu_sub">
-	        <li><a href="#">登入</a></li>
+	        <li><a id="btn_login" href="javascript:;">登入</a></li>
 	        <li><a href="#">魅力值</a></li>
 	        <li><a id="head_member_icon" href="#"><img  src="pic/head_member_icon.svg" alt="會員"></a></li>
 	    </ul>
@@ -41,6 +41,15 @@ function head_html(){
 	            <li><a id="head_member_icon" href="#">會員中心</a></li>
 	        </ul>
 	    </div>
+	</div>
+
+	<div id="login_box" class="login_box">
+		<h2>登入/註冊</h2>
+		<form id="login_form" action="">
+			<input type="e-mail" placeholder="hi@gmail.com">
+			<input type="psw" placeholder="6位數密碼">
+			<input type="submit" value="送出">
+		</form>
 	</div>
 	`;
 	
@@ -124,5 +133,115 @@ function foot_html(){
 }
 
 
+window.addEventListener('load',function(){
 
+
+    //聊天室操控
+    var chatRoom_control = document.getElementById('chatRoom_control');
+    var chatroom_btn_open = document.getElementById('chatroom_btn_open');
+    //取得物件
+    var chatRoom = document.getElementById('chatRoom');
+    var chatboxLeft = document.getElementById('chatboxLeft');
+    var ch = 0,//控制開關，0為初始關閉值，1為打開
+    ch1 =0;
+
+    // console.log(chatboxLeft);
+
+    //聊天室標題被點擊後要顯示出完整視窗，反之已顯示則關閉
+    chatRoom_control.addEventListener('click',function(){
+        if(ch1==0){//如果視窗是關閉狀態，就打開
+            chatRoom.style.cssText="transform:translateY(0%)";
+            chatboxLeft.style.cssText="opacity:0;";
+            return ch1 =1;
+        }else{
+            chatRoom.style.cssText="transform:translateY(77%)";
+            chatboxLeft.style.cssText="opacity:0;"; 
+            return ch1 =0,ch=0;      
+        }
+
+    });
+
+    //聊天室左側區塊被點擊後要顯示出完整視窗，反之已顯示則關閉
+    chatroom_btn_open.addEventListener('click',function(){
+        if(ch==0){//要顯示出來
+            chatboxLeft.style.cssText="opacity:1;";
+            return ch = 1;
+        }else{//要關閉
+            chatboxLeft.style.cssText="opacity:0;";
+            return ch = 0;
+        }
+    });
+
+
+    //手機版聊天室操控_01
+    var btn_chatroom_phone = document.getElementById('btn_chatroom_phone');
+    var chatRoom_phone_part1 = document.getElementById('chatRoom_phone_part1');
+    var chatRoom_phone_part2 = document.getElementById('chatRoom_phone_part2');
+    var fr_demo = document.getElementById('fr_demo');
+    var btn_menu_menu =document.getElementById('btn_menu_menu');
+    var menu_phone = document.getElementById('menu_phone');
+    var btn_chat_prev =document.getElementById('btn_chat_prev');
+
+
+    btn_chatroom_phone.addEventListener('click',function(){
+        if(chatRoom_phone_part1.hasAttribute('value') == false){
+            chatRoom_phone_part1.style.cssText=" top: 8vh;;opacity:1";
+            chatRoom_phone_part2.style.cssText="opacity:0";
+            chatRoom_phone_part1.setAttribute('value','打開');
+        }else{
+            chatRoom_phone_part1.style.cssText=" top: -100vh;;opacity:0";
+            chatRoom_phone_part2.style.cssText="opacity:0";
+            chatRoom_phone_part1.removeAttribute('value','打開');
+        }
+
+    },false);
+
+
+    //手機版聊天室操控_02
+    fr_demo.addEventListener('click',function(){
+        chatRoom_phone_part1.style.cssText=" top:8vh;left:0%;opacity:1; ";
+        chatRoom_phone_part2.style.cssText=" left: 0;display:flex;opacity:1;z-index:4";
+    });
+
+    btn_chat_prev.addEventListener('click',function(){
+        chatRoom_phone_part1.style.cssText=" top:8vh;left:0;opacity:1; ";
+        chatRoom_phone_part2.style.cssText="opacity:0;z-index:-1";
+    },false);
+
+
+    //手機版menu操控
+    btn_menu_menu.addEventListener('click',function(){
+
+        if(menu_phone.hasAttribute('value') == false){
+            menu_phone.style.cssText="transform: scale(1);opacity:1; ";
+            menu_phone.setAttribute('value','打開');
+        }else{
+            menu_phone.style.cssText="transform: scale(0);opacity:0; ";
+            menu_phone.removeAttribute('value','打開');
+        }
+	});
+
+
+
+	//登入燈箱操作
+	var login_box = document.getElementById('login_box');
+	var login_form = document.getElementById('login_form');
+	var btn_login = document.getElementById('btn_login');
+	btn_login.addEventListener('click',function(){
+		
+		login_box.style.cssText = 'opacity:1;z-index:15;';
+
+		login_form.addEventListener('click',function(e){
+			e.stopPropagation();
+		},false);
+
+		login_box.addEventListener('click',function(){
+			login_box.style.cssText = 'opacity:0;z-index:-1;';
+		},false);
+
+		
+
+	},false);
+
+},false);
 
