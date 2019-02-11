0087c031a81c86282344cdced3469fdf0128236d
rt250619 <rt250619@gmail.com>
2019-02-11 21:45:04 +0800

    0211 21:45

---
+++ cartAdd.php
@@ -0,0 +1,8 @@
+<?php 
+session_start();
+$no = $_REQUEST["proNo"];
+$_SESSION["proName"][$no] = $_REQUEST["proName"];
+$_SESSION["price"][$no] = $_REQUEST["price"];
+$_SESSION["img"][$no] = $_REQUEST["img"];
+header("Location:BearMJ_shop_addcart.php");
+?>
