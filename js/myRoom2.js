7cd96fe81c105d8888dfbb79619f897482e3a2b4
chuyun0216 <colleenko0216@gmail.com>
2019-02-16 02:45:15 +0800

    20190216 02:44 我的窩

--- js/myRoom2.js
+++ js/myRoom2.js
@@ -191,5 +191,55 @@ function init(){
 
 	dataReset.onclick = resetForLastTime;
 
+	//衣櫃區
+	function showWardrobeLightBox(){
+		var wardrobe =document.getElementById("wardrobe");
+		var wardrobeLightBox =document.getElementById("wardrobeLightBox");
+		wardrobeLightBox.style.display="";
+	  }
+	  function wardrobeClose(){
+		wardrobeLightBox.style.display="none";
+	  }
+	  function showKeepsaveSuccessLightBox(){
+		var btnSaveChageCloth = document.getElementById("btnSaveChageCloth");
+		var keepSaveClothes = document.getElementById("keepSaveClothes");
+		keepSaveClothes.style.display="";
+	  }
+	  function keepSaveClothesClose(){
+		keepSaveClothes.style.display="none";
+	  }
+	  
+	  function changeHat(e){
+		var hat = e.target;
+		var src = hat.src.replace("hat/hat_","head/head_");
+		document.getElementById("wearhat").src = src;
+	  }
+	  function changeCloth(e){
+		  var cloth = e.target;
+		  var src = cloth.src.replace("cloth/cloth_","body/body_");
+		  document.getElementById("putonCloth").src = src;
+	  }
+	var wardrobe =document.getElementById("wardrobe");
+	wardrobe.onclick =showWardrobeLightBox;
+	
+	var btnClosetCancel =document.getElementById("btnClosetCancel");
+	btnClosetCancel.onclick =wardrobeClose;
+	
+	var btnSaveChageCloth =document.getElementById("btnSaveChageCloth");
+	btnSaveChageCloth.onclick = showKeepsaveSuccessLightBox;
+  
+	var btnkeepSaveClose=document.getElementById("btnkeepSaveClose");
+	btnkeepSaveClose.onclick =keepSaveClothesClose;
+	
+	var imgs = document.getElementsByClassName("hat");
+	for( var i=0; i<imgs.length; i++){
+	  imgs[i].onclick = changeHat;
+	}
+	
+	var clothimgs = document.getElementsByClassName("cloth");
+	for( var i=0; i<=imgs.length; i++){
+	  clothimgs[i].onclick = changeCloth;
+	}
+
 }
 window.onload = init;
