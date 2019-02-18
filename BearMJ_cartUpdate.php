3fea1efffe583267986e34cde993a37e463cb292
rt250619 <rt250619@gmail.com>
2019-02-18 16:51:09 +0800

    0218 1650 購物成功寫進資料庫

--- BearMJ_cartUpdate.php
+++ BearMJ_cartUpdate.php
@@ -1,6 +1,7 @@
 <?php 
 session_start();
 $no = $_REQUEST["proNo"];
+unset($_SESSION["proNo"][$no]);
 unset($_SESSION["proName"][$no]);
 unset($_SESSION["price"][$no]);
 unset($_SESSION["img"][$no]);
--- cartAdd.php
+++ cartAdd.php
@@ -1,6 +1,7 @@
 <?php 
 session_start();
 $no = $_REQUEST["proNo"];
+$_SESSION["proNo"][$no]= $no;
 $_SESSION["proName"][$no] = $_REQUEST["proName"];
 $_SESSION["price"][$no] = $_REQUEST["price"];
 $_SESSION["img"][$no] = $_REQUEST["img"];
