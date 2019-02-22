dd66692329bbf9f802c521faec77ae083f442153
4a390141 <4a390141@stust.edu.tw>
2019-02-22 12:34:16 +0800

    拔換衣服出錯ㄉ扣改掉瞜 ㄏㄏ

--- js/myRoom2.js
+++ js/myRoom2.js
@@ -1,12 +1,12 @@
-function init(){
+function init() {
 
 	var profileLightBox = document.getElementById("profileLightBox");
 	var x = document.getElementById("closeProfile");
 	var computer = document.getElementById("computer");
 
-	function show(){
+	function show() {
 		profileLightBox.style.display = "";
-			if(edit.innerHTML == "編輯"){
+		if (edit.innerHTML == "編輯") {
 
 			edit.innerHTML = "編輯";
 			mName.style.display = "none";
@@ -20,63 +20,63 @@ function init(){
 			hobbyText.style.display = "";
 			selfIntroText.style.display = "";
 		}
-		var xhr=new XMLHttpRequest();
-			xhr.onreadystatechange= function(){
-				if( xhr.readyState == 4){ 
-			        if( xhr.status == 200){ 
-			        // alert(xhr.responseText);
+		var xhr = new XMLHttpRequest();
+		xhr.onreadystatechange = function () {
+			if (xhr.readyState == 4) {
+				if (xhr.status == 200) {
+					// alert(xhr.responseText);
 					var XML = xhr.responseXML;
-					var str0 =(XML.documentElement.childNodes[0].firstChild.nodeValue);
-					var str1 =(XML.documentElement.childNodes[1].firstChild.nodeValue);
-					var str2 =(XML.documentElement.childNodes[2].firstChild.nodeValue);
-					var str3 =(XML.documentElement.childNodes[3].firstChild.nodeValue);
-					var str4 =(XML.documentElement.childNodes[4].firstChild.nodeValue);
-					var str5 =(XML.documentElement.childNodes[5].firstChild.nodeValue);
-					var str6 =(XML.documentElement.childNodes[6].firstChild.nodeValue);
-					var str7 =(XML.documentElement.childNodes[7].firstChild.nodeValue);
-					var str8 =(XML.documentElement.childNodes[8].firstChild.nodeValue);
+					var str0 = (XML.documentElement.childNodes[0].firstChild.nodeValue);
+					var str1 = (XML.documentElement.childNodes[1].firstChild.nodeValue);
+					var str2 = (XML.documentElement.childNodes[2].firstChild.nodeValue);
+					var str3 = (XML.documentElement.childNodes[3].firstChild.nodeValue);
+					var str4 = (XML.documentElement.childNodes[4].firstChild.nodeValue);
+					var str5 = (XML.documentElement.childNodes[5].firstChild.nodeValue);
+					var str6 = (XML.documentElement.childNodes[6].firstChild.nodeValue);
+					var str7 = (XML.documentElement.childNodes[7].firstChild.nodeValue);
+					var str8 = (XML.documentElement.childNodes[8].firstChild.nodeValue);
 					console.log(str6);
 					// var strr = typeof(str6);
 					// console.log(strr);
-					var newStr2 = str2.substring(0,10);
+					var newStr2 = str2.substring(0, 10);
 					var arrStr6 = str6.split("");
-					if(arrStr6[0]==2){
-						arrStr6[0]="";
-					}else{
-						arrStr6[0]="�见�� ";
+					if (arrStr6[0] == 2) {
+						arrStr6[0] = "";
+					} else {
+						arrStr6[0] = "�见�� ";
 					}
-					if(arrStr6[1]==2){
-						arrStr6[1]="";
-					}else{
-						arrStr6[1]="�𤓖敶� ";
+					if (arrStr6[1] == 2) {
+						arrStr6[1] = "";
+					} else {
+						arrStr6[1] = "�𤓖敶� ";
 					}
-					if(arrStr6[2]==2){
-						arrStr6[2]="";
-					}else{
-						arrStr6[2]="�𨺗璅� ";
+					if (arrStr6[2] == 2) {
+						arrStr6[2] = "";
+					} else {
+						arrStr6[2] = "�𨺗璅� ";
 					}
-					if(arrStr6[3]==2){
-						arrStr6[3]="";
-					}else{
-						arrStr6[3]="�寥ㄙ ";
+					if (arrStr6[3] == 2) {
+						arrStr6[3] = "";
+					} else {
+						arrStr6[3] = "�寥ㄙ ";
 					}
-					if(arrStr6[4]==2){
-						arrStr6[4]="";
-					}else{
-						arrStr6[4]="���飛 ";
+					if (arrStr6[4] == 2) {
+						arrStr6[4] = "";
+					} else {
+						arrStr6[4] = "���飛 ";
 					}
-					if(arrStr6[5]==2){
-						arrStr6[5]="";
-					}else{
-						arrStr6[5]="��撠� ";
+					if (arrStr6[5] == 2) {
+						arrStr6[5] = "";
+					} else {
+						arrStr6[5] = "��撠� ";
 					}
 					var newArrStr6 = arrStr6.join("");
-					$("#xhrAcctBack").css("display","").text(str0);
-					$("#mNameText").css("display","").text(str1);
-					$("#constellationText").css("display","").text(newStr2);
-					$("#hobbyText").css("display","").text(newArrStr6);
-					$("#selfIntroText").css("display","").text(str7);
-					
+					$("#xhrAcctBack").css("display", "").text(str0);
+					$("#mNameText").css("display", "").text(str1);
+					$("#constellationText").css("display", "").text(newStr2);
+					$("#hobbyText").css("display", "").text(newArrStr6);
+					$("#selfIntroText").css("display", "").text(str7);
+
 					console.log(localStorage["MEM_no"]);
 
 					$('#mName').val(str1);
@@ -84,27 +84,27 @@ function init(){
 					$('#hobby').val(newArrStr6);
 					$('#selfIntro').val(str7);
 
-			        }else{  
-			          // alert(xhr.status);
-			        }
-			    }else{
-			    	// alert(xhr.readyState);
-			    }
-			}//onready
-			var data_info = "MEM_no=" + localStorage["MEM_no"];
-		    var url = "php/getData.php";
-		    xhr.open("Post",url,true);
-		    xhr.setRequestHeader("content-type","application/x-www-form-urlencoded");
-		    xhr.send(data_info);
-		    // console.log(data_info);	
+				} else {
+					// alert(xhr.status);
+				}
+			} else {
+				// alert(xhr.readyState);
+			}
+		}//onready
+		var data_info = "MEM_no=" + localStorage["MEM_no"];
+		var url = "php/getData.php";
+		xhr.open("Post", url, true);
+		xhr.setRequestHeader("content-type", "application/x-www-form-urlencoded");
+		xhr.send(data_info);
+		// console.log(data_info);	
 	}
-	function close(){
+	function close() {
 		profileLightBox.style.display = "none";
 	}
 	computer.onclick = show;
 	x.onclick = close;
 
-//按下編輯後會發生的事
+	//按下編輯後會發生的事
 	var edit = document.getElementById("change");
 	var dataReset = document.getElementById("dataReset");
 	var mName = document.getElementById("mName");
@@ -123,8 +123,8 @@ function init(){
 	hobby.style.display = "none";
 	selfIntro.style.display = "none";
 	dataReset.style.display = "none";
-	function editDATA(){		
-		if(edit.innerHTML == "編輯"){
+	function editDATA() {
+		if (edit.innerHTML == "編輯") {
 
 			edit.innerHTML = "儲存";
 			mName.style.display = "";
@@ -138,7 +138,7 @@ function init(){
 			// hobbyText.style.display = "none";
 			selfIntroText.style.display = "none";
 			change.disabled = false;
-		}else{
+		} else {
 			edit.innerHTML = "編輯";
 			mName.style.display = "none";
 			constellation.style.display = "none";
@@ -150,39 +150,39 @@ function init(){
 			constellationText.style.display = "";
 			hobbyText.style.display = "";
 			selfIntroText.style.display = "";
-			
-			var xhr=new XMLHttpRequest();
-			xhr.onreadystatechange= function(){
-				if( xhr.readyState == 4){ 
-			        if( xhr.status == 200){ 
-					var XML = xhr.responseXML;
-					var str0 =(XML.documentElement.childNodes[0].firstChild.nodeValue);
-					var str1 =(XML.documentElement.childNodes[1].firstChild.nodeValue);
-					$("#mNameText").css("display","").text(str0);
-					$("#selfIntroText").css("display","").text(str1);
-					localStorage["MEM_nick"] = str0;
-					$(".login_pic + span").text(str0);
-			        }else{  
-			          // alert(xhr.status);
-			        }
-			    }else{
-			    	// alert(xhr.readyState);
-			    }
+
+			var xhr = new XMLHttpRequest();
+			xhr.onreadystatechange = function () {
+				if (xhr.readyState == 4) {
+					if (xhr.status == 200) {
+						var XML = xhr.responseXML;
+						var str0 = (XML.documentElement.childNodes[0].firstChild.nodeValue);
+						var str1 = (XML.documentElement.childNodes[1].firstChild.nodeValue);
+						$("#mNameText").css("display", "").text(str0);
+						$("#selfIntroText").css("display", "").text(str1);
+						localStorage["MEM_nick"] = str0;
+						$(".login_pic + span").text(str0);
+					} else {
+						// alert(xhr.status);
+					}
+				} else {
+					// alert(xhr.readyState);
+				}
 			}//onready
-			var data_info = "MEM_no=" + localStorage["MEM_no"]+"&MEM_nick="+ $(this).parent().parent().find('#memName').val()+"&MEM_like="+ $(this).parent().parent().find('#memHobby').val()+"&MEM_intro="+ $(this).parent().parent().find('#memIntroduce').val();
-		    var url = "php/sendData.php";
-		    xhr.open("Post",url,true);
-		    xhr.setRequestHeader("content-type","application/x-www-form-urlencoded");
-		    xhr.send(data_info);
+			var data_info = "MEM_no=" + localStorage["MEM_no"] + "&MEM_nick=" + $(this).parent().parent().find('#memName').val() + "&MEM_like=" + $(this).parent().parent().find('#memHobby').val() + "&MEM_intro=" + $(this).parent().parent().find('#memIntroduce').val();
+			var url = "php/sendData.php";
+			xhr.open("Post", url, true);
+			xhr.setRequestHeader("content-type", "application/x-www-form-urlencoded");
+			xhr.send(data_info);
 			change.disabled = false;
 		}
 	}
 	change.onclick = editDATA;
 
 
-//按下復原→到一開始的樣子
+	//按下復原→到一開始的樣子
 	var dataReset = document.getElementById("dataReset");
-	function resetForLastTime(){ 
+	function resetForLastTime() {
 		document.getElementById("mName").value = mNameText.innerHTML;
 		document.getElementById("constellation").value = constellationText.innerHTML;
 		document.getElementById("hobby").value = hobbyText.innerHTML;
@@ -192,53 +192,54 @@ function init(){
 	dataReset.onclick = resetForLastTime;
 
 	//衣櫃區
-	function showWardrobeLightBox(){
-		var wardrobe =document.getElementById("wardrobe");
-		var wardrobeLightBox =document.getElementById("wardrobeLightBox");
-		wardrobeLightBox.style.display="";
-	  }
-	  function wardrobeClose(){
-		wardrobeLightBox.style.display="none";
-	  }
-	  function showKeepsaveSuccessLightBox(){
+	function showWardrobeLightBox() {
+		var wardrobe = document.getElementById("wardrobe");
+		var wardrobeLightBox = document.getElementById("wardrobeLightBox");
+		wardrobeLightBox.style.display = "";
+	}
+	function wardrobeClose() {
+		wardrobeLightBox.style.display = "none";
+	}
+	function showKeepsaveSuccessLightBox() {
 		var btnSaveChageCloth = document.getElementById("btnSaveChageCloth");
 		var keepSaveClothes = document.getElementById("keepSaveClothes");
-		keepSaveClothes.style.display="";
-	  }
-	  function keepSaveClothesClose(){
-		keepSaveClothes.style.display="none";
-	  }
-	  
-	  function changeHat(e){
-		var hat = e.target;
-		var src = hat.src.replace("hat/hat_","head/head_");
-		document.getElementById("wearhat").src = src;
-	  }
-	  function changeCloth(e){
-		  var cloth = e.target;
-		  var src = cloth.src.replace("cloth/cloth_","body/body_");
-		  document.getElementById("putonCloth").src = src;
-	  }
-	var wardrobe =document.getElementById("wardrobe");
-	wardrobe.onclick =showWardrobeLightBox;
-	
-	var btnClosetCancel =document.getElementById("btnClosetCancel");
-	btnClosetCancel.onclick =wardrobeClose;
-	
-	var btnSaveChageCloth =document.getElementById("btnSaveChageCloth");
-	btnSaveChageCloth.onclick = showKeepsaveSuccessLightBox;
-  
-	var btnkeepSaveClose=document.getElementById("btnkeepSaveClose");
-	btnkeepSaveClose.onclick =keepSaveClothesClose;
-	
-	var imgs = document.getElementsByClassName("hat");
-	for( var i=0; i<imgs.length; i++){
-	  imgs[i].onclick = changeHat;
+		keepSaveClothes.style.display = "";
 	}
-	
+	function keepSaveClothesClose() {
+		keepSaveClothes.style.display = "none";
+	}
+
+	function changeHat(e) {
+		var hat = e.target;
+		var src = hat.src.replace("hat/hat_", "head/head_");
+		document.getElementById("wearhat").src = src;
+	}
+
+	var wardrobe = document.getElementById("wardrobe");
+	wardrobe.onclick = showWardrobeLightBox;
+
+	var btnClosetCancel = document.getElementById("btnClosetCancel");
+	btnClosetCancel.onclick = wardrobeClose;
+
+	var btnSaveChageCloth = document.getElementById("btnSaveChageCloth");
+	btnSaveChageCloth.onclick = showKeepsaveSuccessLightBox;
+
+	var btnkeepSaveClose = document.getElementById("btnkeepSaveClose");
+	btnkeepSaveClose.onclick = keepSaveClothesClose;
+
+	var imgs = document.getElementsByClassName("hat");
+	for (var i = 0; i < imgs.length; i++) {
+		imgs[i].onclick = changeHat;
+	}
+
 	var clothimgs = document.getElementsByClassName("cloth");
-	for( var i=0; i<=imgs.length; i++){
-	  clothimgs[i].onclick = changeCloth;
+
+	for (var i = 0; i < clothimgs.length; i++) {
+		clothimgs[i].addEventListener('click', (e) => {
+			var cloth = e.target;
+			var src = cloth.src.replace("cloth/cloth_", "body/body_");
+			document.getElementById("putonCloth").src = src;
+		})
 	}
 
 }
