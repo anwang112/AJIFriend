4b9c235c9e522e0a5d5b5c1aa826047616f97956
rt250619 <rt250619@gmail.com>
2019-02-25 11:06:18 +0800

    0225 1106

---
+++ getCoin.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+$data = json_decode($_REQUEST["data"]);
+$errMsg = "";
+try {
+    require_once("connectBooks.php");
+        $sql = "update member set mCoin = (mCoin + :money), last_play = :nowDay where memNo=:memNo;";
+        $Sta = $pdo->prepare($sql);
+        $Sta->bindParam(':money',$data->money);
+        $Sta->bindParam(':memNo', $data->memNo);
+        $Sta->bindParam(':nowDay', $data->nowDay);
+        $Sta->execute();
+
+       echo 1;
+      
+    
+
+} catch (PDOException $e) {
+    echo $e->getMessage();
+    $errMsg .= "錯誤 : " . $e->getMessage() . "<br>";
+    $errMsg .= "行號 : " . $e->getLine() . "<br>";
+}
+?>
