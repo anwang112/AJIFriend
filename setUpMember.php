825537e4380c0d5f2bcecb664c2f2ffa7fdc1b0f
rt250619 <rt250619@gmail.com>
2019-03-01 00:13:32 +0800

    0301 0013

--- setUpMember.php
+++ setUpMember.php
@@ -6,18 +6,18 @@ try {
     require_once("connectBooks.php");
     // if (isset($_SESSION["memId"]) === false) {
     $sql = "INSERT INTO `member` (`memNo`, `memId`, `memPsw`, `mName`, `constellation`, `hobby`, `self-intro` ,`loveGiven`,`mColor`, `eye`, `animal`)
-        values(NULL, 'a', 'a', 'a', '1', '1', '111' ,3 ,'aaaaaa', '1', '1');";
+        values(NULL, :memId, :memPsw, :mName, :constellaton, :hobby, :selfIntro ,3 ,:mColor, :eye, :animal);";
     $createRole = $pdo->prepare($sql);
 
-    // $createRole->bindValue(":memId", $createRoleData->memId);
-    // $createRole->bindValue(":memPsw", $createRoleData->memPsw);
-    // $createRole->bindValue(":mName", $createRoleData->mName);
-    // $createRole->bindValue(":constellaton", $createRoleData->constellation);
-    // $createRole->bindValue(":hobby", $createRoleData->hobby);
-    // $createRole->bindValue(":selfIntro", $createRoleData->selfIntro);
-    // $createRole->bindValue(":mColor", $createRoleData->mColor);
-    // $createRole->bindValue(":eye", $createRoleData->eye);
-    // $createRole->bindValue(":animal", $createRoleData->animal);
+    $createRole->bindValue(":memId", $createRoleData->memId);
+    $createRole->bindValue(":memPsw", $createRoleData->memPsw);
+    $createRole->bindValue(":mName", $createRoleData->mName);
+    $createRole->bindValue(":constellaton", $createRoleData->constellation);
+    $createRole->bindValue(":hobby", $createRoleData->hobby);
+    $createRole->bindValue(":selfIntro", $createRoleData->selfIntro);
+    $createRole->bindValue(":mColor", $createRoleData->mColor);
+    $createRole->bindValue(":eye", $createRoleData->eye);
+    $createRole->bindValue(":animal", $createRoleData->animal);
     $createRole->execute();
     echo '成功';
 
